ed9176975e24fb0562f304b9df50e9ad96a834e9
Alterativity <1124834@hr.nl>
2026-01-30 16:33:36 +0100

    Update Person.cs

--- Week 2/Period 1/W02.1.1T06_Fields-and-constructor/Person.cs
+++ Week 2/Period 1/W02.1.1T06_Fields-and-constructor/Person.cs
@@ -1,3 +1,13 @@
+// Old Code
+class Person
+{
+
+
+    public string GetIntroduction() => $"I am {FirstName} {LastName}";
+}
+
+
+// New Code
 class Person(string FirstName, string LastName)
 {
     public string GetIntroduction() => $"I am {FirstName} {LastName}";
