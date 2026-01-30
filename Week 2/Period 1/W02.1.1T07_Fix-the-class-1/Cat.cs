04e7074ce0f8f2be939193935803a83fbc353187
Alterativity <1124834@hr.nl>
2026-01-30 16:40:09 +0100

    Create Cat.cs

---
+++ Week 2/Period 1/W02.1.1T07_Fix-the-class-1/Cat.cs
@@ -0,0 +1,20 @@
+// Old Code
+class Cat
+{
+    public string Name;
+
+    public Cat(string Name)
+    {
+        Name = Name;
+    }
+}
+// New Code
+class Cat
+{
+    public string Name;
+
+    public Cat(string _Name)
+    {
+        Name = _Name;
+    }
+}
