30019547b32e44d1943137c4dfe319259574b439
Alterativity <1124834@hr.nl>
2026-02-02 16:42:13 +0100

    Create DNA.cs

---
+++ Week 2/Period 2/W02.2.1T09_Class-with-object-of-its-own-type-as-field/DNA.cs
@@ -0,0 +1,30 @@
+class DNA
+{
+    public DNA Ancestor;
+    public string Seq;
+    public DNA(DNA obj, string seq)
+    {
+        Ancestor = obj;
+        Seq = seq.ToUpper();
+    }
+    public DNA Replicate() => new(this, MutateTransition());
+    private static Random _rand = new(0); // Seeded Random
+
+    private string MutateTransition()
+    {
+        int indexToMutate = _rand.Next(0, Seq.Length);
+        string mutatedSeq = Seq[0 .. indexToMutate] // Left of mutation
+            + MutateTransitionTable(Seq[indexToMutate]) // Mutation
+            + Seq[(indexToMutate+1) .. Seq.Length]; // Right of mutation
+        return mutatedSeq;
+    }
+
+    private static char MutateTransitionTable(char nucleotide) => nucleotide switch
+    {
+        'A' => 'G',
+        'G' => 'A',
+        'C' => 'T',
+        'T' => 'C',
+        _ => throw new ArgumentOutOfRangeException($"{nucleotide}", $"Unexpected nucleotide value: {nucleotide}"),
+    };
+}
