01d99a15f2b284ae236a0f0614b713940acaac2b
Alterativity <1124834@hr.nl>
2026-02-02 15:13:12 +0100

    Create Artist.cs

---
+++ Week 2/Period 2/W02.2.1T02_Composing-Songs/Artist.cs
@@ -0,0 +1,10 @@
+class Artist
+{
+    public string Name;
+    public List<Song> ComposedSongs = [];
+
+    public Artist(string name) => Name = name;
+
+    // Your poetry here
+    public void ComposeSong(string title) => ComposedSongs.Add(new Song(Name, title));
+}
