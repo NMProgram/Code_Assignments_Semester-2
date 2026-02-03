26437c0fc13cb899c9e398b5c52a7e46da5a8cdb
Alterativity <1124834@hr.nl>
2026-02-03 15:10:14 +0100

    Create Calculator.cs

---
+++ Week 3/Period 1/W03.1.1T03_Static-classes/Calculator.cs
@@ -0,0 +1,8 @@
+static class Calculator
+{
+    public static double Add(double value1, double value2) => value1 + value2;
+    public static double Subtract(double value1, double value2) => value1 - value2;
+    public static double Multiply(double value1, double value2) => value1 * value2;
+    public static double Divide(double value1, double value2) => value1 / value2;
+    public static double Modulo(double value1, double value2) => value1 % value2;
+}
