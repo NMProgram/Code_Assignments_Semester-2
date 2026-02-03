322e12bf5cb94843abfef759cf31dcf24a20a256
Alterativity <1124834@hr.nl>
2026-02-03 14:18:28 +0100

    Create ShopItem.cs

---
+++ Week 2/Period 2/W02.2.2O4_Grocery-List/ShopItem.cs
@@ -0,0 +1,11 @@
+class ShopItem
+{
+    public string ID, Name;
+    public double Price;
+    public ShopItem(string id, string name, double price)
+    {
+        ID = id;
+        Name = name;
+        Price = price;
+    }
+}
