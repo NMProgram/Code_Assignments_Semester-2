e36401d6d6395e5b491cf86ca0a55588f70d8ae7
Alterativity <1124834@hr.nl>
2026-02-03 16:21:59 +0100

    Create Point.cs

---
+++ Week 3/Period 1/W03.1.2O01_Euclidean-distance/Point.cs
@@ -0,0 +1,11 @@
+class Point
+{
+    public double X, Y;
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public static double GetEuclideanDistance(Point p1, Point p2) =>
+    Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+}
