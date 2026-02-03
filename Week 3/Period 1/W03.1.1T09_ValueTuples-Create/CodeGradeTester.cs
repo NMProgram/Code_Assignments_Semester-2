5a3b905c5ead228bde19ee9c09d23f2c8a71d6d1
Alterativity <1124834@hr.nl>
2026-02-03 16:05:12 +0100

    Update CodeGradeTester.cs

--- Week 3/Period 1/W03.1.1T09_ValueTuples-Create/CodeGradeTester.cs
+++ Week 3/Period 1/W03.1.1T09_ValueTuples-Create/CodeGradeTester.cs
@@ -1,17 +1,47 @@
-static class CodeGradeTester
+// Old Code
+class EmployeeData
 {
-    static void Main()
-    {
-        EmployeeData employeeData = new(
-        [
-            new("Carl Lucas Cage", 55000),
-            new("Clark Joseph Kent", 60000),
-            new("Diana Prince", 60000),
-            new("Peter Benjamin Parker", 50000),
-        ]);
+    private readonly List<Employee> Employees;
+    public EmployeeData(List<Employee> employees) => Employees = employees;
 
-        (decimal, decimal) salaryRange = employeeData.GetSalaryRange();
-        Console.WriteLine("Lowest salary: " + salaryRange.Item1);
-        Console.WriteLine("Highest salary: " + salaryRange.Item2);
+    public List<decimal> GetSalaryRange()
+    {
+        decimal minSalary = decimal.MaxValue;
+        decimal maxSalary = decimal.MinValue;
+
+        foreach (var employee in Employees)
+        {
+            if (employee.Salary < minSalary)
+                minSalary = employee.Salary;
+
+            if (employee.Salary > maxSalary)
+                maxSalary = employee.Salary;
+        }
+
+        return [minSalary, maxSalary];
+    }
+}
+
+// New Code
+class EmployeeData
+{
+    private readonly List<Employee> Employees;
+    public EmployeeData(List<Employee> employees) => Employees = employees;
+
+    public ValueTuple<decimal, decimal> GetSalaryRange()
+    {
+        decimal minSalary = decimal.MaxValue;
+        decimal maxSalary = decimal.MinValue;
+
+        foreach (var employee in Employees)
+        {
+            if (employee.Salary < minSalary)
+                minSalary = employee.Salary;
+
+            if (employee.Salary > maxSalary)
+                maxSalary = employee.Salary;
+        }
+
+        return (minSalary, maxSalary);
     }
 }
