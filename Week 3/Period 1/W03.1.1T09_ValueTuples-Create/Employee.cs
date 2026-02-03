6a19327b45fbfe30c06cc8891e3d0c81bfbe36a4
Alterativity <1124834@hr.nl>
2026-02-03 16:03:42 +0100

    Create Employee.cs

---
+++ Week 3/Period 1/W03.1.1T09_ValueTuples-Create/Employee.cs
@@ -0,0 +1,11 @@
+class Employee
+{
+    public readonly string Name;
+    public decimal Salary;
+
+    public Employee(string name, decimal salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+}
