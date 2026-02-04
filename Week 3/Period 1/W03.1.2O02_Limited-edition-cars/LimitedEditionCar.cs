b1bf527a1137a825f15219637e861401fab5f5b9
Alterativity <1124834@hr.nl>
2026-02-04 12:15:07 +0100

    Update LimitedEditionCar.cs

--- Week 3/Period 1/W03.1.2O02_Limited-edition-cars/LimitedEditionCar.cs
+++ Week 3/Period 1/W03.1.2O02_Limited-edition-cars/LimitedEditionCar.cs
@@ -1,8 +1,8 @@
 class LimitedEditionCar
 {
+    public const string Model = "Mclaren F1";
     public readonly string ChassisNumber;
     public static int Counter = 0;
-    public const string MODEL = "Mclaren F1";
     public LimitedEditionCar()
     {
         Counter++;
