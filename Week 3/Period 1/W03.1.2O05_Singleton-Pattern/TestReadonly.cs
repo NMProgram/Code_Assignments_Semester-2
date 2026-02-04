d25473f25f98da4c777503990c85c702270f471a
Alterativity <1124834@hr.nl>
2026-02-04 15:06:16 +0100

    Create TestReadonly.cs

---
+++ Week 3/Period 1/W03.1.2O05_Singleton-Pattern/TestReadonly.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+
+static class TestReadonly
+{
+    public static void Start()
+    {
+        PrintIsFieldReadOnly(typeof(FinancialCalculator), "Cache");
+    }
+
+    private static void PrintIsFieldReadOnly(Type clsType, string fieldName)
+    {
+        FieldInfo? field = clsType.GetField(fieldName,
+            BindingFlags.Public | BindingFlags.NonPublic |
+            BindingFlags.Instance | BindingFlags.Static);
+
+        if (field is null)
+        {
+            Console.WriteLine($"Field {fieldName} not found in {clsType.Name}");
+            return;
+        }
+
+        Console.WriteLine($"{field.Name} is a read-only field: {field.IsInitOnly}");
+    }
+}
